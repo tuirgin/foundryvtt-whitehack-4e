dfb841addea1e4a869a3b882f9125886737ddc67
atm09st <130640729+atm09st@users.noreply.github.com>
2023-05-19 14:14:57 -0500

    1.4.1
    Updated to version 10.291 of foundry
    Added The Clever class and Knacks
    Changed DEX to AGI
    Changed CON to TGH
    Added crowns
    Changed a bunch of small stuff

---
+++ whitehack4e/module/helpers/itemHelpers.js
@@ -0,0 +1,144 @@
+import { wh3e } from "../config.js";
+import * as c from "../constants.js";
+
+/**
+ * Update encumbrance when actor items change
+ * @param {Object} actor
+ */
+export const updateActorEncumbrance = async (actor) => {
+  const items = actor.items;
+  // Calculate encumbrance
+  let encEquipped = 0;
+  let encStored = 0;
+  const equippedArmour = items.filter((item) => item.type === c.ARMOUR && item.system.equippedStatus === c.EQUIPPED);
+  encEquipped = encEquipped + getEncumbranceForItems(equippedArmour);
+  encEquipped =
+    encEquipped +
+    getEncumbranceForItems(
+      items.filter((item) => item.type === c.WEAPON && item.system.equippedStatus === c.EQUIPPED)
+    );
+  encEquipped =
+    encEquipped +
+    getEncumbranceForItems(
+      items.filter((item) => item.type === c.GEAR && item.system.equippedStatus === c.EQUIPPED)
+    );
+  encStored =
+    encStored +
+    getEncumbranceForItems(
+      items.filter((item) => item.type === c.ARMOUR && item.system.equippedStatus === c.STORED)
+    );
+  encStored =
+    encStored +
+    getEncumbranceForItems(
+      items.filter((item) => item.type === c.WEAPON && item.system.equippedStatus === c.STORED)
+    );
+  encStored =
+    encStored +
+    getEncumbranceForItems(items.filter((item) => item.type === c.GEAR && item.system.equippedStatus === c.STORED));
+
+  await actor.update({
+    system: {
+      encumbrance: {
+        equipped: encEquipped,
+        stored: encStored,
+      },
+    },
+  });
+};
+
+/**
+ * Update AC when actor items change
+ * @param {Object} actor
+ */
+export const updateActorArmourClass = async (actor) => {
+  const items = actor.items;
+  const equippedArmour = items.filter((item) => item.type === c.ARMOUR && item.system.equippedStatus === c.EQUIPPED);
+
+  // Calculate armour class
+  let ac = 0;
+  if (equippedArmour.length > 0) {
+    ac = getArmourClassForItems(equippedArmour);
+  }
+
+  await actor.update({
+    system: {
+      combat: {
+        armourClass: ac,
+      },
+    },
+  });
+};
+
+/**
+ * Update Vocation and Species for actor when actor items change
+ * @param {Object} actor
+ */
+export const updateActorGroups = async (actor) => {
+  const items = actor.items;
+
+  // Get vocation and species
+  const speciesObj = items.filter((item) => item.type === c.ABILITY && item.system.type === c.SPECIES);
+  const vocationObj = items.filter((item) => item.type === c.ABILITY && item.system.type === c.VOCATION);
+  const species = speciesObj.length > 0 ? speciesObj[0].name : game.settings.get("whitehack4e", "defaultSpecies");
+  const vocation = vocationObj.length > 0 ? vocationObj[0].name : c.EMPTYSTRING;
+
+  await actor.update({
+    system: {
+      basics: {
+        vocation: vocation,
+        species: species,
+      },
+    },
+  });
+};
+
+const getArmourClassForItems = (items) => {
+  let maxAc = 0;
+  let modifierAc = 0;
+  items.forEach((item) => {
+    let itemAc = item.system.armourClass;
+    console.log('Item AC', item.system.armourClass);
+    if ([c.PLUSONE, c.PLUSTWO, c.PLUSTHREE, c.MINUSONE, c.MINUSTWO, c.MINUSTHREE].includes(itemAc)) {
+      modifierAc = modifierAc + +(wh3e.armourClasses[itemAc]);
+    } else if (itemAc !== c.SPECIAL) {
+      itemAc = +itemAc;
+      maxAc = itemAc > maxAc ? itemAc : maxAc;
+    }
+  });
+  return maxAc + modifierAc;
+};
+
+const getEncumbranceForItems = (items) => {
+  let encCount = 0;
+  items.forEach((item) => {
+    if (item.type == c.WEAPON || item.type === c.GEAR) {
+      const quantity = item.system.quantity === undefined ? 1 : item.system.quantity;
+      switch (item.system.weight) {
+        case c.REGULAR:
+          encCount = encCount + quantity;
+          break;
+        case c.HEAVY:
+          encCount = encCount + quantity * 2;
+          break;
+        case c.MINOR:
+          encCount = encCount + quantity / 2;
+          break;
+        case c.SMALL:
+          encCount = encCount + quantity / 5;
+          break;
+        case c.NEGLIGIBLE:
+          encCount = encCount + quantity / 100;
+          break;
+        default:
+          encCount = encCount++;
+      }
+    } else {
+      if (item.system.armourClass !== c.SPECIAL) {
+        encCount = encCount + Math.abs(+(wh3e.armourClasses[item.system.armourClass]));
+      } else {
+        encCount = encCount + 1;
+      }
+    }
+  });
+  return encCount;
+};
---
+++ whitehack4e/module/sheets/WH3MonsterSheet.js
@@ -0,0 +1,102 @@
+import { rollModDialog, attackRollDialog } from "../helpers/diceHelpers.js";
+import * as c from "../constants.js";
+
+export default class WH3MonsterSheet extends ActorSheet {
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      template: "systems/whitehack4e/templates/sheets/monster-sheet.hbs",
+      classes: ["wh3e", "sheet", "monster"],
+      width: c.MONSTER_SHEET_WIDTH,
+      height: c.MONSTER_SHEET_HEIGHT,
+      resizable: true,
+    });
+  }
+
+  /**
+   * Fetch Foundry data
+   * @returns {Object}
+   */
+  getData() {
+    const data = super.getData();
+    let monsterData = data.actor;
+    monsterData.config = CONFIG.wh3e;
+    monsterData.hasToken = !(this.token === null);
+    monsterData.editable = this.options.editable;
+    return monsterData;
+  }
+
+  /**
+   * Register event listeners
+   * @param {Object} html
+   */
+  activateListeners(html) {
+    if (this.isEditable) {
+      html.find(".hitDiceBase").change(this._monsterUpdateStats.bind(this));
+    }
+
+    // Owner only listeners
+    if (this.actor.isOwner) {
+      html.find("label.attack-roll").click(this._attackRollHandler.bind(this));
+      html.find("label.savingThrow").click(this._savingThrowRollHandler.bind(this));
+      html.find(".init-label").click(this._initiativeRollHander.bind(this));
+    }
+
+    super.activateListeners(html);
+  }
+
+  /**
+   * Update monster ST and AV based on Hit Dice
+   * @param {Object} event
+   */
+  _monsterUpdateStats(event) {
+    const newHDBase = parseInt(event.currentTarget.value);
+    this.actor.update({
+      system: {
+        savingThrow: newHDBase + 5,
+        combat: {
+          attackValue: newHDBase + 10,
+        },
+      },
+    });
+  }
+
+  /**
+   * Create item to use as weapon for monster and pass to roll dialog
+   */
+  async _attackRollHandler() {
+    // To use the diceHelper.js attackRoll need to create an item
+    // for the monster attack
+
+    // Ensures there is only one attack item for the monster
+    if (this.actor.items.size < 1) {
+      let newItem = {
+        name: this.actor.name,
+        type: c.WEAPON,
+        system: {
+          description: c.EMPTYSTRING,
+          weight: c.REGULAR,
+          rateOfFire: c.NONE,
+        },
+      };
+      await this.actor.createEmbeddedDocuments("Item", [newItem]);
+    }
+    let monsterAttackItem = this.actor.items.filter((item) => item)[0];
+    // Add damage to monsterAttackItem as that may have changed
+    monsterAttackItem.system.damage = this.actor.system.damage;
+    attackRollDialog(monsterAttackItem);
+  }
+
+  /**
+   * Call saving throw dialog
+   */
+  _savingThrowRollHandler() {
+    rollModDialog(this.actor, c.SAVINGTHROW, game.i18n.localize("wh3e.sheet.savingThrow"));
+  }
+
+  /**
+   * Roll initiative for monster
+   */
+  _initiativeRollHander() {
+    this.actor.rollInitiative(this.token);
+  }
+}
